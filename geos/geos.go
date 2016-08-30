d446f56528161d49f514bdacfb31aa67fb7ff752
Alban Peignier <alban.peignier@af83.com>
2016-08-30 12:51:37 +0200

    Replace geos.h by inline code (easier to build)

--- geos/geos.go
+++ geos/geos.go
@@ -5,7 +5,17 @@ package geos
 
 /*
 #cgo LDFLAGS: -lgeos_c
-#include "geos.h"
+
+#include <geos_c.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+void gogeos_notice_handler(const char *fmt, ...);
+void gogeos_error_handler(const char *fmt, ...);
+char *gogeos_get_last_error(void);
+GEOSContextHandle_t gogeos_initGEOS();
 */
 import "C"
 
